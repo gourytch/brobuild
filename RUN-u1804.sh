c8c9d7fd7f0da97f6a3095bf63c8c73c8aec5207
Lev Babchenko <lvbabchenko@yandex-team.ru>
2019-09-05 16:40:02 +0300

    add missing files

--- RUN-u1804.sh
+++ RUN-u1804.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 base=$(dirname $(readlink -f $0))
-io=$base/io
+io=$base/io-u1804
 mkdir -p $io
-docker build -t brobuild $base
-docker run --rm -v $io:/io brobuild bash -c "/build.sh /io"
-docker run --rm -v $io:/io brobuild bash -c "chown -R $(id -u):$(id -g) /io/*"
+docker build -t brobuild:u1804 $base
+docker run -it --rm -v $io:/io brobuild:u1804 bash -c "/build.sh /io"
+docker run -it --rm -v $io:/io brobuild:u1804 bash -c "chown -R $(id -u):$(id -g) /io/*"
 echo "done."
